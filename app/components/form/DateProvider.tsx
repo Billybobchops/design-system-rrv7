4e918b4de9395fd43b2533b27a3c4a7621c478aa
Billybobchops <chlanda52@gmail.com>
2025-01-20 11:08:25 -0500

    chore(deps): update @mui/x-date-pickers and @mui/x-date-pickers/AdapterDayjs to latest versions

--- app/components/form/DateProvider.tsx
+++ app/components/form/DateProvider.tsx
@@ -14,3 +14,20 @@ const DateProvider: React.FC<DateProviderProps> = ({ children }) => {
 };
 
 export default DateProvider;
+
+// import { LocalizationProvider } from '@mui/x-date-pickers';
+// import AdapterDayjs from '@mui/x-date-pickers/AdapterDayjs';
+
+// interface DateProviderProps {
+//     children?: React.ReactNode;
+// }
+
+// const DateProvider: React.FC<DateProviderProps> = ({ children }) => {
+//     return (
+//         <LocalizationProvider dateAdapter={AdapterDayjs as unknown as new (...args: any) => any}>
+//             {children}
+//         </LocalizationProvider>
+//     );
+// };
+
+// export default DateProvider;
