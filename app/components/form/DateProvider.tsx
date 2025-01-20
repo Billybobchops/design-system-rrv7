b488e689ecde5710636a3a7d07e7a8dd318cb613
Billybobchops <chlanda52@gmail.com>
2025-01-20 15:30:38 -0500

    comment out all datepicker code

--- app/components/form/DateProvider.tsx
+++ app/components/form/DateProvider.tsx
@@ -1,20 +1,24 @@
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 // const { AdapterDayjs } = await import('@mui/x-date-pickers/AdapterDayjs'); // dynamic import to sidestep bundler resolution problems
 
-interface DateProviderProps {
-    children?: React.ReactNode;
-}
+// interface DateProviderProps {
+//     children?: React.ReactNode;
+// }
 
-const DateProvider: React.FC<DateProviderProps> = ({ children }) => {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            {children}
-        </LocalizationProvider>
-    );
-};
+// const DateProvider: React.FC<DateProviderProps> = ({ children }) => {
+//     return (
+//         <LocalizationProvider dateAdapter={AdapterDayjs}>
+//             {children}
+//         </LocalizationProvider>
+//     );
+// };
 
-export default DateProvider;
+// export default DateProvider;
+
+
+
 
 // import { LocalizationProvider } from '@mui/x-date-pickers';
 // import AdapterDayjs from '@mui/x-date-pickers/AdapterDayjs';
